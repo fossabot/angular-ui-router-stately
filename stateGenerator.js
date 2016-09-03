072f8c409b10909015baa18959e01b0a631b73f5
BobChao87 <bobchao87@gmail.com>
2016-09-03 15:56:57 -0700

    stateGenerator makes valid states
    stateGenerator now makes fully indented and valid $stateProvider code as output.

--- stateGenerator.js
+++ stateGenerator.js
@@ -12,20 +12,24 @@ module.exports = stateGenerator;
 /**
  * @function stateGenerator
  * 
- * @param {number} [states=100] Number of states to generate.
+ * @param {number<Integer>} [states=100] Integer number of states to generate.
  * @param {string[]} [requiredStates=['alpha', 'omega']] States that must be present.
  * @returns {string} String that is valid AngularJS UI Router code to make this state structure.
  */
 function stateGenerator(states, requiredStates) {
-  states = states || 100;
-  requiredStates = requiredStates || ['alpha', 'omega']; 
+  states = Number.isInteger(states) && states > 0 ? states : 100;
+  requiredStates = Array.isArray(requiredStates) ? requiredStates : ['alpha', 'omega']; 
   var numStates = 1;
   var stateStructure = {};
   var stateReferences = [stateStructure];
   var stateNames = 'abcdefghijklmnopqrstuvwxyz'.split('');
   for (var i = states - requiredStates.length; i > 0; i--) {
-    var state = stateNames[Math.floor(Math.random() * 26)];
-    var position = Math.floor(Math.random() * numStates);
+    // Always look up one state, but only assign if it's not an overlap.
+    // We do a full reroll to avoid bias and potential filling problems.
+    do {
+      var state = stateNames[Math.floor(Math.random() * 26)];
+      var position = Math.floor(Math.random() * numStates);
+    } while (state in stateReferences[position]);
     stateReferences[position][state] = {};
     stateReferences[numStates] = stateReferences[position][state];
     numStates++;
@@ -36,7 +40,29 @@ function stateGenerator(states, requiredStates) {
     stateReferences[numStates] = stateReferences[position][state];
     numStates++;
   });
-  return JSON.stringify(stateStructure, null, ' ');
+  return stateStringGenerator(stateStructure) + ';';
+}
+
+/**
+ * @function stateStringGenerator
+ * 
+ * @param {Object} stateStructure Generated structure to convert to string.
+ * @param {string} [stateString='$stateProvider\n'] Current set of states thus far.
+ * @param {string} [parentState=''] Current parent state for all entities. 
+ * @return Current set of states after iteration.
+ */
+function stateStringGenerator(stateStructure, stateString, parentState) {
+  stateString = stateString || '$stateProvider';
+  parentState = parentState || '';
+  var stateNameLong;
+  for (var stateName in stateStructure) {
+    stateNameLong = (parentState ? parentState + '.' : '') + stateName;
+    stateString += "\n  .state(\'" + stateNameLong + "', {\n"
+      + "    url: '/" + stateName + "',\n"
+      + '  })';
+    stateString = stateStringGenerator(stateStructure[stateName], stateString, stateNameLong);
+  }
+  return stateString;
 }
 
 console.log(stateGenerator(1000, ['alpha', 'beta', 'gamma']));
